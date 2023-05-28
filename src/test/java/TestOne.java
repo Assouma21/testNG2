a5daf82de853fac52023a6576ca5714ebf0ec910
Assouma21 <sou_ssouma@hotmail.com>
2023-05-28 12:37:47 -0400

    commit

--- src/test/java/TestOne.java
+++ src/test/java/TestOne.java
@@ -1,4 +1,5 @@
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
 public class TestOne {
@@ -7,12 +8,43 @@ public class TestOne {
     public void beforeMethod(){
         System.out.println("Before Method");
     }
-    @Test
+    @Test(priority = 2)
     public void test1(){
         System.out.println("test 1");
     }
-    @Test
-    public void test2(){
-        System.out.println("test 2");
+    @Test(priority = 0)
+    public void test9(){
+        System.out.println("test9");
     }
+
+    @Test
+    public void test5(){
+        System.out.println("test5");
+    }
+   // @Test(dataProvider = "provider1",dataProviderClass = dataProviderHelper.class)
+    //public void test2(int ssn , String name , int ph){
+      //  System.out.println("SSN is" + ssn);
+        //System.out.println("Name is" + name);
+        //System.out.println("phone number is " + ph);
+
+    //}
+    //@Test(dataProvider = "provider")
+    //public void test3(int ssn , String name ){
+        //System.out.println("SSN is" + ssn);
+      //  System.out.println("Name is" + name);
+   // }
+    @DataProvider(name = "provider")
+    public Object[][] dataProvider(){
+        Object[][] data = new Object[2][2];
+        data[0][0] = 123;
+        data[0][1] = "kasif";
+        data[1][0] = 236;
+        data[1][1] = "Asma";
+
+        return data;
+    }
+
+
+
+
 }
--- src/test/java/dataProviderHelper.java
+++ src/test/java/dataProviderHelper.java
@@ -1,2 +1,21 @@
-package PACKAGE_NAME;public class dataProviderClass {
+import org.testng.annotations.DataProvider;
+
+public class dataProviderHelper {
+    @DataProvider(name= "provider1")
+    public Object[][] dataProvider(){
+        Object[][] newdata = new Object[3][3];
+        newdata[0][0]= 123;
+        newdata[0][1]= "Shuvashish";
+        newdata[0][2]= 1234567890;
+        newdata[1][0]= 1223;
+        newdata[1][1]= "halim";
+        newdata[1][2]= 3479623;
+        newdata[2][0]= 1523;
+        newdata[2][1]= "Aziz";
+        newdata[2][2]= 1236523;
+
+        return newdata;
+
+    }
+
 }
